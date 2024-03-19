c2c45eec74ee249a2aee916b77f48a1a3e9fe10d
vasyl polazhynets <riofftest03@gmail.com>
2024-03-19 09:27:15 +0100

    ,
    ,

---
+++ pole.py
@@ -0,0 +1,18 @@
+cislo = int(1) #
+cislo_float = float(3.23) #
+text = str("string je sada znaku. napřiklad pro text")
+boolean = True #boolenska hodnota, znači bud pravdu/nepravdu (true/false)
+
+#Ddatovy typ - pole, kde mužeme mit vice prvku. Zavorku pišeme pravy alt + d/f
+#v poli se začina na pozici 0. ačkoliv je delka pole 6, posledni prvek je na pozici 5
+    
+            #0  #1  #2  #3  #4  #5
+arrays = ["Ford", "porsche", "audi", "bmw", "mercedes"]
+
+#tohle vypiše, uplně cele pole včetně hranatych závorek
+#a uvozovek s čarkama
+print(arrays)
+
+#vypiše pouze jeden prvek bez uvozovek, čarek 
+#
+print(arrays[1])
